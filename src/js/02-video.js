f258d0c242a8e2b961c9065320e73b8ba21e7370
GabiKovtun <gabi.tovt@gmail.com>
2023-02-23 16:39:35 +0100

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const vimeoPlayer = new Player('vimeo-player');
+
+vimeoPlayer.on('timeupdate', throttle(() => {
+    vimeoPlayer.getCurrentTime().then((seconds) => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000));
+
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  vimeoPlayer.setCurrentTime(currentTime);
+}
 
