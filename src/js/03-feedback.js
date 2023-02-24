d8123caf1f844312da7f4c64b02f0877cd87e1af
GabiKovtun <gabi.tovt@gmail.com>
2023-02-24 14:20:04 +0100

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const feedbackKey = 'feedback-form-state';
+const submitButton = form.querySelector('button[type="submit"]');
+
+const sendFeedback = e => {
+  e.preventDefault();
+  const inputEl = {
+    emailInput: emailInput.value,
+    messageInput: messageInput.value,
+  };
+  localStorage.setItem(feedbackKey, JSON.stringify(inputEl));
+  emailInput.value = '';
+  messageInput.value = '';
+};
+const savedMessages = localStorage.getItem(feedbackKey);
+console.log(savedMessages);
+
+form.addEventListener('submit', throttle(sendFeedback, 500));
+
 
