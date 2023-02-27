49485bf20a22172561a53d5c47a0679ec4d32397
GabiKovtun <gabi.tovt@gmail.com>
2023-02-27 11:26:26 +0100

    update 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,8 @@ import throttle from 'lodash.throttle';
 
 const vimeoPlayer = new Player('vimeo-player');
 
-vimeoPlayer.on('timeupdate', throttle(() => {
-    vimeoPlayer.getCurrentTime().then((seconds) => {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    });
+vimeoPlayer.on('timeupdate', throttle((e) => {
+localStorage.setItem('videoplayer-current-time', e.seconds);
   }, 1000));
 
   const currentTime = localStorage.getItem('videoplayer-current-time');
